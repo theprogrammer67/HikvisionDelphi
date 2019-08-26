fee97c07a30bec0eae6e0ec20fd95352d838ac4a
Стовпец Игорь <stoi@rarus.ru>
2019-08-26 22:55:42 +0300

    add: Добавить Callback функцийю рисования поверх видео

--- common/uVideoWindow.pas
+++ common/uVideoWindow.pas
@@ -3,7 +3,7 @@
 interface
 
 uses uCHCNetSDK, Vcl.Controls, Winapi.Windows, uHikvisionErrors, System.Classes,
-  Vcl.Graphics, System.SysUtils;
+  Vcl.Graphics, System.SysUtils, System.Generics.Collections;
 
 type
   TVideoWindow = class(TCustomControl)
@@ -11,20 +11,44 @@ type
     FChannel: Integer;
     FRealHandle: Integer;
     FOverlayText: string;
+    FFontSize: Integer;
+    FFontName: TFontName;
+    FFontColor: TColor;
+  private
+    class var FObjects: TObjectList<TVideoWindow>;
+    procedure RegisterObj;
+    procedure UnRegisterObj;
+  public
+    class constructor Create;
+    class destructor Destroy;
+  private
     function GetIsPlaying: Boolean;
+    procedure PrintNoVideo;
+  private
+    class procedure DrawFun(lRealHandle: LongInt; hDc: IntPtr; dwUser: UINT);
+      stdcall; static;
+    procedure DrawFunction(hDc: IntPtr);
+  protected
+    procedure Paint; override;
   public
     constructor Create(AParent: TWinControl); reintroduce;
     destructor Destroy; override;
   public
     procedure Play(AUserID: Integer);
     procedure Stop;
+    procedure SetText;
   public
     property Channel: Integer read FChannel write FChannel;
     property OverlayText: string read FOverlayText write FOverlayText;
     property IsPlaying: Boolean read GetIsPlaying;
+    property FontName: TFontName read FFontName write FFontName;
+    property FontSize: Integer read FFontSize write FFontSize;
+    property FontColor: TColor read FFontColor write FFontColor;
   end;
 
 implementation
+
+uses System.Types;
 
 { TWideoWindow }
 
@@ -34,17 +58,60 @@ begin
   Parent := AParent;
   FRealHandle := -1;
   Color := clNavy;
+
+  FFontName := 'Courier New';
+  FFontSize := 12;
+  FFontColor := RGB(160, 255, 150);
+
+  RegisterObj;
+end;
+
+class constructor TVideoWindow.Create;
+begin
+  FObjects := TObjectList<TVideoWindow>.Create(False);
 end;
 
 destructor TVideoWindow.Destroy;
 begin
   Stop;
+  UnRegisterObj;
   inherited;
+end;
+
+class destructor TVideoWindow.Destroy;
+begin
+  FreeAndNil(FObjects);
+end;
+
+class procedure TVideoWindow.DrawFun(lRealHandle: Integer; hDc: IntPtr;
+  dwUser: UINT);
+var
+  LObj: TVideoWindow;
+begin
+  for LObj in FObjects do
+    if LObj.FRealHandle = lRealHandle then
+    begin
+      LObj.DrawFunction(hDc);
+      Break;
+    end;
+end;
+
+procedure TVideoWindow.DrawFunction(hDc: IntPtr);
+begin
+
 end;
 
 function TVideoWindow.GetIsPlaying: Boolean;
 begin
   Result := FRealHandle >= 0;
+end;
+
+procedure TVideoWindow.Paint;
+begin
+  inherited;
+  if IsPlaying then
+    Exit;
+  PrintNoVideo;
 end;
 
 procedure TVideoWindow.Play(AUserID: Integer);
@@ -64,6 +131,34 @@ begin
   FRealHandle := NET_DVR_RealPlay_V40(AUserID, LPreviewInfo, nil, 0);
   if FRealHandle < 0 then
     RaiseLastHVError;
+  if not NET_DVR_RigisterDrawFun(FRealHandle, DrawFun, 0) then
+    RaiseLastHVError;
+end;
+
+procedure TVideoWindow.PrintNoVideo;
+var
+  LRect: TRect;
+const
+  LText: string = 'NO VIDEO';
+begin
+  Canvas.Font.Size := 12;
+  Canvas.Font.Name := 'Courier New';
+  Canvas.Font.Color := RGB(160, 150, 255);
+  Canvas.Brush.Style := bsClear;
+
+  LRect := Rect(0, 0, Width, Height);
+  DrawText(Canvas.Handle, LText, Length(LText), LRect, DT_SINGLELINE or
+    DT_CENTER or DT_VCENTER);
+end;
+
+procedure TVideoWindow.RegisterObj;
+begin
+  FObjects.Add(Self);
+end;
+
+procedure TVideoWindow.SetText;
+begin
+
 end;
 
 procedure TVideoWindow.Stop;
@@ -71,6 +166,11 @@ begin
   if FRealHandle >= 0 then
     NET_DVR_StopRealPlay(FRealHandle);
   FRealHandle := -1;
+end;
+
+procedure TVideoWindow.UnRegisterObj;
+begin
+  FObjects.Remove(Self);
 end;
 
 end.
